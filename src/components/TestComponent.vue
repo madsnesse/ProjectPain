22bf8be1f8fd62badc5cb88461a37ec2521c6658
madsnesse <mbnesse@hotmail.com>
2021-02-04 14:05:58 +0100

    set up project

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="test">
+        <h1>Heines hathjørne</h1>
+        <p id="jeff">My name jeff</p>
+        <p id="matias">{{msg}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TestComponent',
+    props: {
+        msg: String
+    }
+}
+</script>
+<style scoped>
+    h1{
+        font-family: "Comic Sans MS", "Comic Sans", cursive;
+    }
+    #jeff{
+        width: 50%;
+        color: bisque;
+
+    }
+    #matias{
+        width: 50%;
+        color: rebeccapurple;
+        outline: red;
+    }
+</style>
