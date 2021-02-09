39384a430cbfd515f6669b8fef8f72dc62df9f10
madsnesse <mbnesse@hotmail.com>
2021-02-09 15:19:14 +0100

    imported bootstrap correctly

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <HelloWorld />
+</template>
+<script>
+import HelloWorld from './HelloWorld.vue'
+export default {
+  components: { SelectionMenu, BodyRenderedHelloWorld },
+    name: 'TestComponent',
+    props: {
+
+    }
+}
+</script>
+<style scoped>
+    
+</style>
