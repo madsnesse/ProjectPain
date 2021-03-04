84abfc61c7543d29ffa8694aa3f72dcc2d773400
madsnesse <mbnesse@hotmail.com>
2021-03-04 16:44:13 +0100

    ignore this

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    
-    <hello-world msg="Welcome to Your Vue.js App"></hello-world>
+  <div id="app">    
     <hello-world msg="Welcome to Your Vue.js App"></hello-world>
   </div>
 </template>
@@ -12,6 +9,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 
 export default {
+  
+  counter: 0,
+  methods: {
+    addCount: function (amount) {
+      self.counter += amount
+      console.log(self.counter)
+    }
+  },
   name: 'App',
   components: {
     HelloWorld
