633f6de0d288f7f5243b47e0f7e8a3e88abafe39
larsuib <lju009@uib.no>
2021-03-11 14:29:27 +0100

    Implemented screen-scalability

--- src/javascript/pain_visualizer.js
+++ src/javascript/pain_visualizer.js
@@ -36,11 +36,17 @@ class PainCircle {
 
 export function pain_visualize(p5) {
   let bodyImage;        // reference to body image
+  const imgRatio = 490/1280;
   let painCircle;       // used for rendering pain circle(s)
-  let backgroundLayer;  // back-most layer, actually draw circles on this
+  let bg_canvas;        // back-most canvas, actually draw circles on this
   let usingUI = false;  // don't render while using UI
-  let aspect = 14/9;    // aspect ratio
+  let aspect = 9/14;    // aspect ratio, should have two - one for desktop, one for mobile
   var canvas;           // reference to the p5 canvas
+
+  // Scaling variables
+  let width_div;  // width of parent div
+  let w, h;  // width and height of canvas DOM
+  let x, y;  // x and y pos of canvas DOM
 
   // Debug
   var relativeLabel;
@@ -53,119 +59,124 @@ export function pain_visualize(p5) {
   var radiusSlider;
   var resetButton;
 
-  function canvProp() {  // get canvas properties
-    let canvas_rect = canvas.elt.getBoundingClientRect();
-    return {w:canvas_rect.width,h:canvas_rect.height, x:canvas_rect.x, y:canvas_rect.y};
-  }
-
   p5.preload = function() {
-    const img = require("@/assets/body.png")  // thanks to https://stackoverflow.com/a/65872755
+    let img = require("@/assets/body.png")  // thanks to https://stackoverflow.com/a/65872755
     bodyImage = p5.loadImage(img);  // todo resize image - too large atm
-    console.log("Image \"@/assets/body.png\" loaded. ("+bodyImage.width+"x"+bodyImage.height+")");
   }
 
   p5.setup = function() {
     // get width of parent div
-    let width = document.getElementById("pain_visualizer").offsetWidth;
+    width_div = document.getElementById("pain_visualizer").offsetWidth;
 
-    canvas = p5.createCanvas(width, width*aspect);
-    canvas.elt.style.border = "solid 2px black";
+    // Create canvas
+    canvas = p5.createCanvas(width_div, width_div/aspect);
+    canvas.elt.style.border = "solid 1px black";
+    canvas.elt.style.background = "#ffffff";
     canvas.parent("pain_visualizer");  // Attach to <div>
 
-    createBackground();
-
     // Fetching canvas properties
-    let prop = canvProp();
-    let w = prop.w, h = prop.h, x = prop.x, y = prop.y;
+    let canvas_rect = canvas.elt.getBoundingClientRect();
+    w = canvas_rect.width, h = canvas_rect.height;
+    x = canvas_rect.x, y = canvas_rect.y;
 
-    // Debug
-    relativeLabel = p5.createP("Relative: (x, y)");
-    relativeLabel.style("font-size", "10pt");
-    relativeLabel.position(x+w/100, y+h/100, 70);
+    bg_canvas = p5.createGraphics(w, h);
+    resetBackground();
 
+    // Create UI elements
+    relativeLabel = p5.createP();
     // Size slider
     radiusSliderLabel = p5.createP("Size");
-    radiusSliderLabel.position(x+2.3*w/10, y+h-(1.6*h/10), 70);
     radiusSlider = p5.createSlider(0, 125, 40);
-    radiusSlider.style("width", "200px");
-    radiusSlider.style("height", "40px");
-    radiusSlider.position(x+0.3*w/10, y+h-(1.3*h/10), 70);
     radiusSlider.mouseOver(() => { usingUI = true; });
     radiusSlider.mouseOut(() => { usingUI = false; });
-
     // Reset button
     resetButton = p5.createButton("Reset");
-    resetButton.style("width", "200px");
-    resetButton.style("height", "30px");
-    resetButton.position(x+0.3*w/10, y+h-(0.7*h/10), 70);
     resetButton.mouseOver(() => { usingUI = true; });
     resetButton.mouseOut(() => { usingUI = false; });
-    resetButton.mouseClicked(() => { reset() });
+    resetButton.mouseClicked(() => { resetBackground() });
+
+    updateUI();
 
     // Creating pain circle obj.
     painCircle = new PainCircle(p5, "static", 60);
   }
 
-  function createBackground() {
-    // Fetching canvas properties
-    let prop = canvProp();
-    let w = prop.w, h = prop.h;
-    backgroundLayer = p5.createGraphics(w, h);
-    backgroundLayer.background(240);
-    backgroundLayer.image(bodyImage, w/3.8, h/40, w-w/1.8, (w-w/1.8)*3);
+  function resetBackground() {
+    bg_canvas.clear();
+    let imgWidth = 1.20*w
+    bg_canvas.image(bodyImage, w/3.8, h/15, imgWidth*imgRatio, imgWidth);
   }
 
-  p5.mouseClicked = function() {
-    /* Saving render */
-    if (!usingUI) {
-      console.log("Mouse clicked at ("+p5.mouseX+", "+p5.mouseY+")");
-      painCircle.saveCircle(backgroundLayer);
-    }
-  }
+  function updateValues() {
+    // width of parent div
+    width_div = document.getElementById("pain_visualizer").offsetWidth;
 
-  function reset() {
-    backgroundLayer.clear();
-    createBackground();
+    // Updated scaling variables
+    let canvas_rect = canvas.elt.getBoundingClientRect();
+    w = canvas_rect.width, h = canvas_rect.height;
+    x = canvas_rect.x, y = canvas_rect.y;
+
+    relativeX = Math.round(100*(p5.mouseX / w));  // normalize to 0 to 100 %  scale
+    relativeY = Math.round(100*(p5.mouseY / h));  // normalize to 0 to 100 %  scale
+    relativeR = Math.round(100*(radiusSlider.value() / 125));  // normalize to 0 to 100 %  scale
+
+    // Update values from UI
+    painCircle.updateRadius(radiusSlider.value());
   }
 
   p5.draw = function() {
-      // Update values from UI
-      painCircle.updateRadius(radiusSlider.value());
+    updateValues();
 
-      // Fetching canvas properties
-      let prop = canvProp();
-      let w = prop.w, h = prop.h;
+    // Clear & render background
+    p5.clear();
+    p5.image(bg_canvas, 0, 0);
 
-      // Debug
-      relativeX = Math.round(100*(p5.mouseX / w));  // normalize to 0 to 100 %  scale
-      relativeY = Math.round(100*(p5.mouseY / h));  // normalize to 0 to 100 %  scale
-      relativeR = Math.round(100*(radiusSlider.value() / 125));  // normalize to 0 to 100 %  scale
-      relativeLabel.elt.innerHTML = "Relative: (x="+relativeX+", y="+relativeY+", r="+relativeR+")";
+    relativeLabel.elt.innerHTML = "Relative: (x="+relativeX+", y="+relativeY+", r="+relativeR+")";
 
-      // Clear & render background
-      p5.clear();
-      p5.image(backgroundLayer, 0, 0);
+    // Render circle for top layer if not using UI
+    if (!usingUI) {
+      painCircle.render();
+    }
+  }
 
-      // Render circle for top layer if not using UI
-      if (!usingUI) {
-        painCircle.render();
-      }
+  /* Resizes, repositions and renders UI */
+  function updateUI() {
+    let font_size = w/35+"pt";
+    radiusSliderLabel.position(x+2.5*w/10, y+h-(1.6*h/10), 70);
+    radiusSliderLabel.style("font-size", font_size);
+
+    radiusSlider.position(x+0.3*w/10, y+h-(1.3*h/10), 70);
+    radiusSlider.style("width", w/1.8+"px");
+    radiusSlider.style("height", h/20+"px");
+
+    resetButton.position(x+0.3*w/10, y+h-(0.7*h/10), 70);
+    resetButton.style("width", w/1.8+"px");
+    resetButton.style("height", h/20+"px");
+    resetButton.style("font-size", font_size);
+
+    relativeLabel.position(x+w/100, y+h/100, 70);
+    relativeLabel.style("font-size", font_size);
+  }
+
+  ///////////////////////////////////////////////////
+  //// EVENTS BELOW                              ////
+  //////////////////////////////////////////////////
+  p5.mouseClicked = function() {
+    /* Saving render */
+    if (!usingUI) {
+      console.log("Mouse clicked at ("+Math.round(p5.mouseX)+", "+Math.round(p5.mouseY)+")");
+      painCircle.saveCircle(bg_canvas);
+    }
   }
 
   p5.windowResized = function() {
-    console.log("Window resized.");
-    let width = document.getElementById("pain_visualizer").offsetWidth;
-    p5.resizeCanvas(width, width*aspect);
-    backgroundLayer.resizeCanvas(w, w*aspect);
-    createBackground();
+    updateValues();
 
-    // Fetching canvas properties
-    let prop = canvProp();
-    let w = prop.w, h = prop.h, x = prop.x, y = prop.y;
+    // Resize canvas and background_canvas
+    p5.resizeCanvas(width_div, width_div/aspect);
+    bg_canvas.resizeCanvas(width_div, width_div/aspect);
+    resetBackground();
 
-    radiusSliderLabel.position(x+2.3*w/10, y+h-(1.6*h/10), 70);
-    radiusSlider.position(x+0.3*w/10, y+h-(1.3*h/10), 70);
-    resetButton.position(x+0.3*w/10, y+h-(0.7*h/10), 70);
-    relativeLabel.position(x+w/100, y+h/100, 70);
+    updateUI();
   }
 }
