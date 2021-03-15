c5a0383567347612602b4a5f72e115a7da2176a2
MSunde99 <Matias.Sunde@Student.uib.no>
2021-03-15 15:11:02 +0100

    Made the slider more dynamic

--- src/components/Painstrength.vue
+++ src/components/Painstrength.vue
@@ -1,16 +1,35 @@
 <template>
     <b-container>
-        <b-row align-h="center">
-            <span id="test"></span>
-            1<input type="range" min="1" max="5" value="3" class="slider" id="sli">5
-        </b-row>
-        <b-row align-h="center">
-            <span id="test1"></span>
-            1<input type="range" min="1" max="5" value="3" class="slider" id="sli">5
-        </b-row>
-        <b-row align-h="center">
-            <span id="test2"></span>
-            1<input type="range" min="1" max="5" value="3" class="slider" id="sli">5
-        </b-row>
+        <b-col>
+            <b-row align-h="center">
+                <span id="test2"></span>
+                Weak<input type="range" min="1" max="5" class="slider" id="painstr" v-model="index">Strong
+            </b-row>
+            <b-row align-h="center">
+                {{ out }}
+            </b-row>
+        </b-col>
     </b-container>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            index: 3,
+            pain: ["Light tickle", "Kinda annoying", "this isn't good", "Ouch squared", "help"],
+            out: ""
+        }
+    },
+    watch: {
+        index: {
+            handler() {
+                this.out = this.pain[this.index - 1]
+            }
+        }
+    },
+    mounted: function(){
+        this.out = this.pain[this.index - 1]
+    }
+}
+</script>
