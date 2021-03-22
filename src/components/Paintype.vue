4f9281af03050bd49e2b49b9dff936797205dab5
Sunde99 <Matias.Sunde@Student.uib.no>
2021-03-22 22:23:42 +0100

    Updated the checkboxes
    Fixed formatting, they blue now

--- src/components/Paintype.vue
+++ src/components/Paintype.vue
@@ -2,39 +2,81 @@
     <b-container class="my-buttons">
         <b-row align-h="center">
 
+            <b-form-group
+                v-slot="{ ariaDescribedby }"
+                >
+                <b-form-checkbox-group
+                    class = "m-2"
+                    :aria-describedby="ariaDescribedby"
+                    v-model="left_group"
+                    stacked
+                    buttons
+                    :button-variant="'info'"
+                >
+                    <b-form-checkbox class="my-1" value="Temporal" name="check-button">
+                        Temporal
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Spatial" name="check-button">
+                        Spatial
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Punctate" name="check-button">
+                        Punctate pressure
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Incisive" name="check-button">
+                        Incisive pressure
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Constrictive" name="check-button">
+                        Constrictive pressure
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="temps" name="check-button">
+                        Temporary buttons
+                    </b-form-checkbox>
+                
+                </b-form-checkbox-group>
+            </b-form-group>
+            <b-form-group
+                v-slot="{ ariaDescribedby }"
+                >
+                <b-form-checkbox-group
+                    class = "m-2"
+                    :aria-describedby="ariaDescribedby"
+                    v-model="right_group"
+                    stacked
+                    buttons
+                    :button-variant="'info'"
+                >
+                    <b-form-checkbox class="my-1" value="Traction" name="check-button">
+                        Traction pressure
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Thermal" name="check-button">
+                        Thermal
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Brightness" name="check-button">
+                        Brightness
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Sensory" name="check-button">
+                        Sensory
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Evaluative" name="check-button">
+                        evaluative
+                    </b-form-checkbox>
+
+                    <b-form-checkbox class="my-1" value="Hello" name="check-button">
+                        Hello there
+                    </b-form-checkbox>
+                </b-form-checkbox-group>
+            </b-form-group>
+
             
-
-            <b-form-checkbox v-model="checked1" name="check-button" button :button-variant="!checked1 ? 'secondary' : 'success'" size="">
-                Button Checkbox
-            </b-form-checkbox>
-
-            <b-form-checkbox v-model="checked2" name="check-button" button :button-variant="!checked2 ? 'secondary' : 'success'">
-                Button Checkbox
-            </b-form-checkbox>
-
-            <b-form-checkbox v-model="checked3" name="check-button" button :button-variant="!checked3 ? 'secondary' : 'success'">
-                Button Checkbox
-            </b-form-checkbox>
-
-            <b-form-checkbox v-model="checked4" name="check-button" button :button-variant="!checked4 ? 'secondary' : 'success'">
-                Button Checkbox
-            </b-form-checkbox>
-
-            <b-form-checkbox v-model="checked5" name="check-button" button :button-variant="!checked5 ? 'secondary' : 'success'">
-                Button Checkbox
-            </b-form-checkbox>
-
-            <b-form-checkbox v-model="checked6" name="check-button" button :button-variant="!checked6 ? 'secondary' : 'success'">
-                Button Checkbox
-            </b-form-checkbox>
-
-            <b-form-checkbox v-model="checked7" name="check-button" button :button-variant="!checked7 ? 'secondary' : 'success'">
-                Button Checkbox
-            </b-form-checkbox>
-
-            <b-form-checkbox v-model="checked8" name="check-button" button :button-variant="!checked8 ? 'secondary' : 'success'">
-                Button Checkbox
-            </b-form-checkbox>
 
         </b-row>
         <b-row align-h="center">
@@ -53,15 +95,31 @@
 export default {
     data() {
         return {
-            checked1: false,
-            checked2: false,
-            checked3: false,
-            checked4: false,
-            checked5: false,
-            checked6: false,
-            checked7: false,
-            checked8: false
+            left_group: [], // Must be an array reference!
+            right_group: [] // Must be an array reference!
+            // options_one: [
+            //     { text: 'Temporary', value: 'temporary' },
+            //     { text: 'Temporal', value: 'temporal' },
+            //     { text: 'Spatial', value: 'spatial' },
+            //     { text: 'Punctate Pressure', value: 'punctate_pressure' },
+            //     { text: 'Another Button', value: 'another_button' },
+            //     { text: 'The', value: 'the' }
+            // ],
+            // options_two: [
+            //     { text: 'Buttons', value: 'buttons' },
+            //     { text: 'Tension', value: 'tension' },
+            //     { text: 'Autonomic', value: 'autonomic' },
+            //     { text: 'You get the point', value: 'you' },
+            //     { text: 'Button', value: 'button' },
+            //     { text: 'Hello there', value: 'hello_there' }
+            // ]
         }
     }
 }
 </script>
+
+<style scoped>
+    .buttons{
+        width: 50%;
+    }
+</style>
