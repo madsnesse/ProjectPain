efa3c2b9756239ba4e680a7a64f9cdcf23ea44cc
larsuib <lju009@uib.no>
2021-03-23 04:34:17 +0100

    Fixed UI-bug. Also: fixed scaling & boundary checks. UI-components created in html for bug-fix and easier code.

--- src/components/PainVisualizer.vue
+++ src/components/PainVisualizer.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <p id = "relative"></p>
-    <div id="pain_visualizer"></div>
+  <div id="parent">
+    <div id="canvas"></div>
 
+    <input id="radiusSlider" type="range" min="1" max="100" value="25">
+    <button id="resetButton">Reset</button>
     <h3>Thanks to</h3>
     <a href="https://www.freepik.com/vectors/education">Education vector created by brgfx - www.freepik.com</a>
   </div>
@@ -12,181 +13,160 @@
 <script>
 // thanks to https://medium.com/js-dojo/experiment-with-p5-js-on-vue-7ebc05030d33
 // for general setup of p5 + vue
+const p5_lib = require('p5');
+
 export default {
     name: "PainVisualizer",
-    mounted() {
-      const p5_lib = require('p5');
-      
+    created() {
+      const pain_visualize = p5 => {
+        // Constants
+        const aspectImage = 490/1280;
+        const aspectCanvas = 9/14;
+
+        // p5 variables
+        let bodyImage;        // reference to body image
+        let bg_canvas;        // back-most canvas, actually draw circles on this
+        var canvas;           // reference to the p5 canvas
+
+        // Pain Circle
+        let circleRadius = 30;  // Relative: from 0 to 100
+        let pain_type = "static";
+
+        // Size and positional variables
+        let width_div;  // width of parent div
+        let w, h;       // width and height of canvas DOM
+
+        // Relative units
+        let rx;  // canvas width / 100
+        let ry;  // canvas height / 100
+
+        // UI
+        var parent;
+        var radiusSlider;
+        var resetButton;
+
+        p5.preload = function() {
+          let img = require("@/assets/body.png")  // thanks to https://stackoverflow.com/a/65872755
+          bodyImage = p5.loadImage(img);  // todo resize image - too large atm
+        }
+
+        p5.setup = function() {
+          // get width of parent div
+          parent = document.getElementById("parent");
+
+          // Create canvas
+          canvas = p5.createCanvas(parent.offsetWidth, parent.offsetWidth/aspectCanvas);
+          canvas.elt.style.border = "solid 1px black";
+          canvas.elt.style.background = "#ffffff";
+          canvas.parent("canvas");  // Attach to <div>
+
+          // Fetching canvas properties
+          let canvas_rect = canvas.elt.getBoundingClientRect();
+          w = canvas_rect.width, h = canvas_rect.height;
+          rx = w/100;  // normalize to 0 to 100 scale
+          ry = h/100;  // normalize to 0 to 100  scale
+
+          bg_canvas = p5.createGraphics(w, h);
+          resetBackground();
+
+          // Get UI elements
+          radiusSlider = document.getElementById("radiusSlider");
+          resetButton = document.getElementById("resetButton");
+          resetButton.onclick = function(){resetBackground()};
+        }
+
+        function resetBackground() {
+          bg_canvas.clear();
+          let imgWidth = 1.20*w
+          bg_canvas.image(bodyImage, 26*rx, 5*ry, imgWidth*aspectImage, imgWidth);
+        }
+
+        function updateValues() {
+          circleRadius = radiusSlider.value;
+
+          // width of parent div
+          width_div = document.getElementById("canvas").offsetWidth;
+
+          // Updated scaling variables
+          let canvas_rect = canvas.elt.getBoundingClientRect();
+          w = canvas_rect.width, h = canvas_rect.height;
+
+          // Update relative variables
+          rx = w/100;
+          ry = h/100;
+        }
+
+        /* Renders a circle based on type of pain.
+         * Use this method unless saving circle on background.
+         */
+        function drawCircle() {
+          switch (pain_type) {
+            case "static":
+              p5.fill(255, 0, 0, 150);
+              p5.circle(p5.mouseX, p5.mouseY, circleRadius*w/200);
+              break;
+
+            default:
+              console.error("Non-valid render type.");
+          }
+        }
+
+        p5.draw = function() {
+          updateValues();
+
+          // Clear & render background
+          p5.clear();
+          p5.image(bg_canvas, 0, 0);
+
+          let mx = p5.mouseX / w;  // rel. mouse pos., 0 to 1
+          let my = p5.mouseY / h;
+
+          if (0 <= mx && mx <= 1 && 0 <= my && my <= 1) {  // bounds check
+            drawCircle();
+          }
+        }
+
+        ///////////////////////////////////////////////////
+        //// EVENTS BELOW                              ////
+        //////////////////////////////////////////////////
+        p5.mouseClicked = function() {
+          console.log("mouse event @ abs("+Math.round(p5.mouseX)+", "+Math.round(p5.mouseY)+") rel(x="+Math.round(100*(p5.mouseX / w))+", y="+Math.round(100*(p5.mouseY / h))+", r="+circleRadius+")");
+
+          // Save circle to background
+          let mx = p5.mouseX / w;  // rel. mouse pos., 0 to 1
+          let my = p5.mouseY / h;
+          if (0 <= mx && mx <= 1 && 0 <= my && my <= 1) {  // bounds check
+            bg_canvas.fill(255, 0, 0, 150);
+            bg_canvas.circle(p5.mouseX, p5.mouseY, circleRadius*w/200);
+          }
+        }
+
+        p5.windowResized = function() {
+          updateValues();
+          console.log(parent.offsetWidth);
+          // Resize canvas and background_canvas
+          p5.resizeCanvas(width_div, width_div/aspectCanvas);
+          bg_canvas.resizeCanvas(width_div, width_div/aspectCanvas);
+          resetBackground();
+        }
+      }
+
+      // thanks to https://stackoverflow.com/a/61855707
+      // for setting up p5 in an vue component
       new p5_lib(pain_visualize);
     },
 }
-function pain_visualize(p5) {
-  // Constants
-  const aspectImage = 490/1280;
-  const aspectCanvas = 9/14;
-
-  // p5 variables
-  let bodyImage;        // reference to body image
-  let bg_canvas;        // back-most canvas, actually draw circles on this
-  let usingUI = false;  // don't render while using UI
-  var canvas;           // reference to the p5 canvas
-
-  // Pain Circle
-  let circleRadius = 30;  // Relative: from 0 to 100
-  let pain_type = "static";
-
-  // Size and positional variables
-  let width_div;  // width of parent div
-  let w, h;       // width and height of canvas DOM
-  let x, y;       // x and y pos of canvas DOM
-
-  // Relative units, each an integer from 0 to 100
-  // e.g. Point(10*rx, 0) => x=1/10 of the width, y=0
-  let rx;  // canvas width / 100
-  let ry;  // canvas height / 100
-
-  // UI
-  var debugLabel;
-  var radiusSliderLabel;
-  var radiusSlider;
-  var resetButton;
-  p5.preload = function() {
-    let img = require("@/assets/body.png")  // thanks to https://stackoverflow.com/a/65872755
-    bodyImage = p5.loadImage(img);  // todo resize image - too large atm
-  }
-
-  p5.setup = function() {
-    // get width of parent div
-    width_div = document.getElementById("pain_visualizer").offsetWidth;
-
-    // Create canvas
-    canvas = p5.createCanvas(width_div, width_div/aspectCanvas);
-    canvas.elt.style.border = "solid 1px black";
-    canvas.elt.style.background = "#ffffff";
-    canvas.parent("pain_visualizer");  // Attach to <div>
-
-    // Fetching canvas properties
-    let canvas_rect = canvas.elt.getBoundingClientRect();
-    w = canvas_rect.width, h = canvas_rect.height;
-    x = canvas_rect.x, y = canvas_rect.y;
-    rx = w/100;  // normalize to 0 to 100 scale
-    ry = h/100;  // normalize to 0 to 100  scale
-
-    bg_canvas = p5.createGraphics(w, h);
-    resetBackground();
-
-    // Create UI elements
-    debugLabel = p5.createP();
-    // Size slider
-    radiusSliderLabel = p5.createP("Size");
-    radiusSlider = p5.createSlider(1, 100, circleRadius);
-    radiusSlider.mouseOver(() => { usingUI = true; });
-    radiusSlider.mouseOut(() => { usingUI = false; });
-    // Reset button
-    resetButton = p5.createButton("Reset");
-    resetButton.mouseOver(() => { usingUI = true; });
-    resetButton.mouseOut(() => { usingUI = false; });
-    resetButton.mouseClicked(() => { resetBackground() });
-
-    updateUI();
-  }
-
-  function resetBackground() {
-    bg_canvas.clear();
-    let imgWidth = 1.20*w
-    bg_canvas.image(bodyImage, 26*rx, 5*ry, imgWidth*aspectImage, imgWidth);
-  }
-
-  function updateValues() {
-    circleRadius = radiusSlider.value();
-
-    // width of parent div
-    width_div = document.getElementById("pain_visualizer").offsetWidth;
-
-    // Updated scaling variables
-    let canvas_rect = canvas.elt.getBoundingClientRect();
-    w = canvas_rect.width, h = canvas_rect.height;
-    x = canvas_rect.x, y = canvas_rect.y;
-
-    // Update relative variables
-    rx = w/100;
-    ry = h/100;
-  }
-
-  /* Renders a circle based on type of pain.
-   * Use this method unless saving circle on background.
-   */
-  function drawCircle() {
-    switch (pain_type) {
-      case "static":
-        p5.fill(255, 0, 0, 150);
-        p5.circle(p5.mouseX, p5.mouseY, circleRadius*w/200);
-        break;
-
-      default:
-        console.error("Non-valid render type.");
-    }
-  }
-
-  p5.draw = function() {
-    updateValues();
-
-    // Clear & render background
-    p5.clear();
-    p5.image(bg_canvas, 0, 0);
-
-    //debugLabel.elt.innerHTML = "Relative: (x="+Math.round(100*(p5.mouseX / w))+", y="+Math.round(100*(p5.mouseY / h))+", r="+circleRadius+")";
-    document.getElementById("relative").innerHTML = "Relative: (x="+Math.round(100*(p5.mouseX / w))+", y="+Math.round(100*(p5.mouseY / h))+", r="+circleRadius+")";
-    // Render circle for top layer if not using UI
-    if (!usingUI) {
-      drawCircle();
-    }
-  }
-
-  /* Resizes, repositions and renders UI */
-  function updateUI() {
-    let font_size = rx*3+"pt";
-    radiusSliderLabel.position(x+(25*rx), y+(86*ry));
-    radiusSliderLabel.style("font-size", font_size);
-
-    radiusSlider.position(x+(3*rx), y+(90*ry));
-    radiusSlider.style("width", 50*rx+"px");
-    radiusSlider.style("height", 2*ry+"px");
-
-    resetButton.position(x+(3*rx), y+(93*ry));
-    resetButton.style("width", 50*rx+"px");
-    resetButton.style("height", 5*ry+"px");
-    resetButton.style("font-size", font_size);
-
-    debugLabel.position(x+(rx), y);
-    debugLabel.style("font-size", font_size);
-  }
-
-  ///////////////////////////////////////////////////
-  //// EVENTS BELOW                              ////
-  //////////////////////////////////////////////////
-  p5.mouseClicked = function() {
-    /* Saving render */
-    if (!usingUI) {
-      console.log("Mouse clicked at ("+Math.round(p5.mouseX)+", "+Math.round(p5.mouseY)+")");
-      // Save circle to background
-      bg_canvas.fill(255, 0, 0, 150);
-      bg_canvas.circle(p5.mouseX, p5.mouseY, circleRadius*w/200);
-    }
-  }
-
-  p5.windowResized = function() {
-    updateValues();
-
-    // Resize canvas and background_canvas
-    p5.resizeCanvas(width_div, width_div/aspectCanvas);
-    bg_canvas.resizeCanvas(width_div, width_div/aspectCanvas);
-    resetBackground();
-
-    updateUI();
-  }
-}
-
 </script>
 
-<style></style>
+<style>
+p, input, button {
+  width: 100%;
+}
+p {
+  margin-left: 45%;
+  margin-right:auto;
+}
+#parent {
+  width: 100%;
+}
+</style>
