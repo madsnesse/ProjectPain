9f25d7bbc452494baf9286edd17b071f09c4d84c
Sunde99 <59199436+Sunde99@users.noreply.github.com>
2021-04-09 14:51:29 +0200

    Added an empty settings page

---
+++ src/components/Settings.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container id="main">
+    <b-row align-h="center" class="my-5">
+      Not ready yet
+      <router-link tag="b-button" class="buttons" to="/home">back</router-link>
+    </b-row>
+  </b-container>
+</template>
+
+
+<script>
+export default {
+  name: "Settings",
+};
+</script>
