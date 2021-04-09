d9d76c1af04672c378915950eeca947e7cb99e64
Sunde99 <59199436+Sunde99@users.noreply.github.com>
2021-04-09 14:58:15 +0200

    More buttons

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,8 +1,23 @@
 <template>
   <b-container id="main">
+    <b-row align-h="center" class="my-5"
+      ><h1 id="welcome">It's ya boy, Settings page here</h1></b-row
+    >
     <b-row align-h="center" class="my-5">
       Not ready yet
-      <router-link tag="b-button" class="buttons" to="/home">back</router-link>
+      <router-link tag="b-button" class="buttons" to="/home"
+        >Change Password</router-link
+      >
+    </b-row>
+    <b-row align-h="center" class="my-5">
+      <router-link tag="b-button" class="buttons" to="/home"
+        >Recalibrate</router-link
+      >
+    </b-row>
+    <b-row align-h="center" class="my-5">
+      <router-link tag="b-button" class="buttons" to="/home"
+        >Go back</router-link
+      >
     </b-row>
   </b-container>
 </template>
