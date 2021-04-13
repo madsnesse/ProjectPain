dbc8855dfa4a69704a32961501c16d501bc5899e
madsnesse <Mads.Nesse@student.uib.no>
2021-04-13 11:09:21 +0200

    added dropdown

--- src/components/Painchange.vue
+++ src/components/Painchange.vue
@@ -18,9 +18,13 @@
             </b-col>
         </b-row>
         <b-row class="my-3">
-            <b-col><b-button>hei</b-button></b-col>
-            <b-col><b-button>du</b-button></b-col>
+            <b-col> 
+                <b-form-select v-model="current" :options="options">
+                    
+                </b-form-select>
+            </b-col>
         </b-row>
+        <b-row>{{ selected }}</b-row>
     </b-container>
 </template>
 
@@ -31,10 +35,54 @@ export default {
         return {
             one: false,
             two: false,
-            three: false
+            three: false,
+            noSelected: 0,
+            selected: [],
+            sel: [false,false,false],
+            current: 0,
+            options: [
+                { value:  0 , text: 'placeholder',disabled:true},
+                { value:  1 , text:'liquor'},
+                { value:  2 , text: 'stimulants such as coffee'},
+                { value:  3 , text: 'eating' },
+                { value:  4 , text: 'heat' },
+                { value:  5 , text: 'cold' },
+                { value:  6 , text: 'damp' },
+                { value:  7 , text: 'weather changes' },
+                { value:  8 , text: 'massage or use of a vibrator' },
+                { value:  9 , text: 'pressure' },
+                { value:  10 , text: 'no movement' },
+                { value:  11 , text: 'movement' },
+                { value:  12 , text: 'lying down' },
+                { value:  13 , text: 'distraction (TV reading etc.)' },
+                { value:  14 , text: 'urination or defecation' },
+                { value:  15 , text: 'tension' },
+                { value:  16 , text: 'bright lights' },
+                { value:  17 , text: 'loud noises' },
+                { value:  18 , text: 'going to work' },
+                { value:  19 , text: 'intercourse' },
+                { value:  20 , text: 'mild exercise' },
+                { value:  21 , text: 'fatigue' },
+                { value:  22 , text: 'intercourse' }
+            ]
+        }
+    },watch:{
+        current: function(){
+            this.selectMult(this.current)
+            
         }
     },
     methods: {
+        selectMult: function(select) {
+            console.log(select)
+            if (this.noSelected <= 3){
+                this.$set(this.options, select, Object.assign(this.options[select],{disabled:true}))
+                this.$set(this.sel, select,)
+                this.selected = this.selected.concat(select)
+            }
+            
+            //selecteded.$set(selecteded,select)
+        },
         toggleOne: function () {
             this.one = !this.one
             this.two = false
