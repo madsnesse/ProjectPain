de038ebe3f6bf14e9d7ce8c40168271e81e4d08b
Sunde99 <59199436+Sunde99@users.noreply.github.com>
2021-04-20 14:10:49 +0200

    Added placeholder

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,6 +1,8 @@
 <template>
     <b-container>
-        <b-row align-h="center" class="my-5"><h1 id="welcome">History!</h1></b-row>
+        <b-row align-h="center" class="mt-2"><h1 id="welcome">History!</h1></b-row>
+        <b-row align-h="center" class="mt-4"><img src="../assets/Placeholder.png" class="picture"/></b-row>
+        <b-row align-h="center" class="mt-2 mb-4">Slider here</b-row>
         <b-row>
             <b-col>
                 <router-link tag="b-button" class="buttons" to="/Profile">Go back</router-link>
